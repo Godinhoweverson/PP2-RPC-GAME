6c314e5242be3a5109fdf28eec5fc32514be3272
Godinhoweverson <>
2023-05-19 20:38:56 +0000

    get all attribute on game.html used dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
-console.log("hello");
+'use strict';
+
+// Get all attribute that will be used and store in Variable
+
+const btnReset = document.querySelector('#btn-reset');
+const imagePlayer = document.querySelector('#image--player');
+const imageComputer = document.querySelector('#image--computer');
+const btnsBox = document.querySelectorAll('#btn-box');
+const scorePlayer = document.querySelector('#score-player');
+const scoreComputer = document.querySelector('#score-computer');
+
+//Computer choice
+const computerChoice = ['rock', 'paper', 'scissors'];
+
+
+let message;
+let computer;
+let player;
+
+/**
+ * 
+ */
+for (buttons of computerChoice) {
+    buttons.addEventListener('click', function () {
+        let itens = this.getAttribute('data-choice');
+        gameSteUp();
+
+    });
+}
+
+function gameSteUp (itens){
+    
+}
