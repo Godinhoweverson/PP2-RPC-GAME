fd63c67e8e2cc1d3f592739d01f083c8b2a8b043
Godinhoweverson <>
2023-05-20 09:20:23 +0000

    create if statement for check who win the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,16 @@
 // Get all attribute that will be used and store in Variable
 
 const btnReset = document.querySelector('#btn-reset');
+const message = document.querySelector('h1');
 const imagePlayer = document.querySelector('#image--player');
 const imageComputer = document.querySelector('#image--computer');
 const btnsBox = document.querySelectorAll('.btn-box');
 const scorePlayer = document.querySelector('#score-player');
 const scoreComputer = document.querySelector('#score-computer');
 
-
-console.log(btnsBox);
-
 //Computer choice
 const computerChoice = ['rock', 'paper', 'scissors'];
 
-
-let message;
 let computer;
 let player;
 
@@ -52,6 +48,25 @@ function gameSteUp(itens) {
     // Set the source imagePlayer based on data-choice
     imagePlayer.src = `assets/images/${computerChoice[itens]}.png`;
 
+
+    computer = computerChoice[randomNumber];
+    player = computerChoice[itens];
+
+
+
+    if (player === computer) {
+        message.textContent = 'Draw!';
+
+    } else if (player === 'rock' && computer === 'scissors') {
+        message.textContent = 'Player win!';
+    } else if (player === 'scissors' && computer === 'paper') {
+        message.textContent = 'Player win!';
+
+    } else if (player === 'paper' && computer === 'rock') {
+        message.textContent = 'Player win!';
+
+    } else {
+        message.textContent = 'Computer win!';
+    }
+
 }
-
-
